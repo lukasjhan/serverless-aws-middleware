5ec13f0b41aadb85493f47f80dc8a1c518d75520
Lukas.J.Han <lukas.j.han@gmail.com>
2023-05-07 12:07:29 +0900

    [#13]Add Typeorm instead of raw mysql

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,22 +2,22 @@ import build from './build';
 
 import aws from './aws';
 import logger from './logger';
-import mysql from './mysql';
 import trace from './trace';
 import tempFile from './tempFile';
+import db from './db';
 
 export const middleware = {
   build,
   aws,
   trace,
   logger,
-  mysql,
   tempFile,
+  db,
 };
 
 export * from './base';
 export * from './aws';
 export * from './trace';
 export * from './logger';
-export * from './mysql';
 export * from './tempFile';
+export * from './db';
